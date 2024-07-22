2022c2593d2f380b3c5ba9e2f31bf59f605433bf
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-22 10:59:23 +0200

    Working getComputerChoice function
    In order for the game to work, this function is a key part.
    Now it randomly returns rock,paper,scissors, and prints it
    using console.log().

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
-console.log("Hello World!")
-console.log("How are you?")
+//Randomly return rock/paper/scissors
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+function getComputerChoice(){
+    let choice = Math.random();
+
+    if(choice <= 0.33) { console.log(rock); return rock}
+    else if (choice > 0.33 && choice <= 0.66) { console.log(paper);return paper}
+    else {console.log(scissors); return scissors}
+
+}
+
+getComputerChoice();
