59647f9c22c42e31209e7a05f76eb3850193f042
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-22 11:42:15 +0200

    Round Logic is now working with case unsensitive
    User can input rock ROck or RocK and it will work correctly as expected.
    Currently you can play 1 round, will continue to make the game up to
    5 turns.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,57 @@
 //Randomly return rock/paper/scissors
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
 
-function getComputerChoice(){
+let humanChoice;
+let computerChoice;
+
+//score variables
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+   
+    if(humanChoice == computerChoice) {
+        console.log("Both used " + humanChoice + ". It's a draw!");
+    }
+    else if(humanChoice == paper  && computerChoice == rock || humanChoice == paper && computerChoice == scissors || humanChoice == scissors && computerChoice == rock) {
+        console.log(computerChoice + " beats " + humanChoice + ". You lose!");
+        console.log("Your Score = " + humanScore + " Computer Score = " + ++computerScore);
+    }
+    else {
+        console.log(humanChoice + " beats " + computerChoice + ". You win!"); 
+        console.log("Your Score = " + ++humanScore + " Computer Score = " + computerScore);
+    }
+}
+
+function getComputerChoice() {
     let choice = Math.random();
 
-    if(choice <= 0.33) { console.log(rock); return rock}
-    else if (choice > 0.33 && choice <= 0.66) { console.log(paper);return paper}
-    else {console.log(scissors); return scissors}
+    if(choice <= 0.33) { 
+        console.log(rock);
+        return rock
+    }
+
+    else if (choice > 0.33 && choice <= 0.66) {
+        console.log(paper);
+        return paper
+    }
+
+    else {
+        console.log(scissors); return scissors
+    }
 
 }
 
-getComputerChoice();
+function getHumanChoice(){
+    return prompt("What is your choice?").toLowerCase();    
+}
+
+humanChoice = getHumanChoice();
+
+computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
