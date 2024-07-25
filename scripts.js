8548085fcef61efd50f37fbb25d8c376acbcca4e
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-07-25 10:00:17 +0200

    Finished buttons and game is playable
    You can play the game in an infinite loop, and will get feedback on
    screen.

--- scripts.js
+++ scripts.js
@@ -13,6 +13,8 @@ let computerScore = 0;
 
 let round = 0;
 
+const body = document.querySelector("body");
+
 function playRound(humanChoice) {
 
     computerChoice = getComputerChoice();
@@ -20,15 +22,29 @@ function playRound(humanChoice) {
     console.log("Round " + ++round);
    
     if(humanChoice == computerChoice) {
+        writeResult("Both used " + humanChoice + ". It's a draw!");
+
         console.log("Both used " + humanChoice + ". It's a draw!");
     }
-    else if(humanChoice == paper  && computerChoice == rock || humanChoice == paper && computerChoice == scissors || humanChoice == scissors && computerChoice == rock) {
+    else if(humanChoice == rock  && computerChoice == paper || humanChoice == paper && computerChoice == scissors || humanChoice == scissors && computerChoice == rock) {
+        
+        ++computerScore;
+
+        writeResult(computerChoice + " beats " + humanChoice + ". You lose!");
+        writeResult("Your Score = " + humanScore + " Computer Score = " + computerScore);
+        
         console.log(computerChoice + " beats " + humanChoice + ". You lose!");
-        console.log("Your Score = " + humanScore + " Computer Score = " + ++computerScore);
+        console.log("Your Score = " + humanScore + " Computer Score = " + computerScore);
     }
     else {
+
+        ++humanScore;
+
+        writeResult(humanChoice + " beats " + computerChoice + ". You win!"); 
+        writeResult("Your Score = " + humanScore + " Computer Score = " + computerScore);
+        
         console.log(humanChoice + " beats " + computerChoice + ". You win!"); 
-        console.log("Your Score = " + ++humanScore + " Computer Score = " + computerScore);
+        console.log("Your Score = " + humanScore + " Computer Score = " + computerScore);
     }
 
     playGame();
@@ -72,24 +88,48 @@ function playGame(){
     }
 */
 
-    if(humanScore == 5){
-        console.log("You got 5 points. You win the game!")
+    if(humanScore >= 5){
+        console.log("You got 5 points. You win the game!");
+        restartGame();
     }
 
-    else if(computerScore == 5){
-        console.log("Computer got 5 points. You lose the game!")
+    else if(computerScore >= 5){
+        console.log("Computer got 5 points. You lose the game!");
+        restartGame();
     }
+}
+
+function writeResult(string){
+
+    let div = document.createElement("div");
+    div.textContent = string;
+    body.appendChild(div);
+
+}
+
+function restartGame(){
+    humanScore = 0;
+    computerScore = 0;
 }
 
 const rockBtn = document.querySelector("#rockBtn");
 
 
 rockBtn.addEventListener("click", () => {
-    humanChoice = getHumanChoice("Rock");
+    humanChoice = getHumanChoice(rock);
 });
 
-const paperBtn = document.querySelector("paperBtn");
-const scissorsBtn = document.querySelector("scissorsBtn");
+const paperBtn = document.querySelector("#paperBtn");
+
+paperBtn.addEventListener("click", () => {
+    humanChoice = getHumanChoice(paper);
+});
+
+const scissorsBtn = document.querySelector("#scissorsBtn");
+
+scissorsBtn.addEventListener("click", () => {
+    humanChoice = getHumanChoice(scissors);
+});
 
 
 //playGame();
